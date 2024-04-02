d623143a3890e629aef114757a7b3c9e4f263af6
Jonathan Lima <Jonathan.lima@bit.com.br>
2024-04-02 11:35:49 -0300

    <feat> desabilitando campo selecione no select do formulario

--- src/components/CustomSelect/index.jsx
+++ src/components/CustomSelect/index.jsx
@@ -10,7 +10,7 @@ const CustomSelect = (props) => {
         <div className='select-input'>
             <label>{ props.label }</label>
             <select onChange={onSelectChange} value={props.value}>
-                <option value="">Selecione</option>
+                <option value="" disabled>Selecione</option>
                 {props.options.map((opcao) => {
                     return <option value={opcao.key} key={opcao.key}>{ opcao.nome }</option>
                 })}
