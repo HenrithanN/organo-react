e8ba5c20e62fe9f8904e9a01ffc2985a674d81cb
Jonathan Lima <Jonathan.lima@bit.com.br>
2024-04-04 10:56:29 -0300

    <feat> instalação da biblioteca uuidv4

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Banner from "./components/Banner";
 import CustomForm from "./components/CustomForm";
 import Time from "./components/Time";
 import Rodape from "./components/Rodape";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [arrayTimes, setTimes] = useState([
-    { key: 'A', nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
-    { key: 'B', nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
-    { key: 'C', nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
-    { key: 'D', nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
-    { key: 'E', nome: 'UX e Design', corPrimaria: '#D86EBF', corSecundaria: '#FAE5F5' },
-    { key: 'F', nome: 'Mobile', corPrimaria: '#FEBA05', corSecundaria: '#FFF5D9' },
-    { key: 'G', nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' }
+    { key: uuidv4(), nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
+    { key: uuidv4(), nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
+    { key: uuidv4(), nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
+    { key: uuidv4(), nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
+    { key: uuidv4(), nome: 'UX e Design', corPrimaria: '#D86EBF', corSecundaria: '#FAE5F5' },
+    { key: uuidv4(), nome: 'Mobile', corPrimaria: '#FEBA05', corSecundaria: '#FFF5D9' },
+    { key: uuidv4(), nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' }
   ]);
   const [ usuariosValue, setUsuarios ] = useState([]);
 
